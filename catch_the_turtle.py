3e773da236f37ee6f5a2786e8ed06b44818daa63
Ahmet Yaşar Sarıkaya <sarikayaahmetyasar9@gmail.com>
2026-02-26 02:57:36 +0300

    Catch the Turtle update

--- catch_the_turtle.py
+++ catch_the_turtle.py
@@ -9,8 +9,7 @@ grid_size = 15
 score = 0
 game_over = False
 
-#turtle list
-turtle_list = []
+turtles = turtle.Turtle()
 
 #score turtle
 score_turtle = turtle.Turtle()
@@ -28,41 +27,28 @@ def setup_score_turtle():
     score_turtle.goto(0, y)
     score_turtle.write(arg="Score: 0", move=False, align="center", font=FONT)
 
-def make_turtle(x, y):
-    t = turtle.Turtle()
+def setup_turtles():
+    turtles.shape("turtle")
+    turtles.shapesize(2,2)
+    turtles.color("green")
+    turtles.penup()
 
-    def handle_click(x, y):
+    def hand_click(x,y):
         global score
         score += 1
-        score_turtle.clear()
-        score_turtle.write(arg="Score: {}".format(score), move=False, align="center", font=FONT)
+        turtles.clear()
+        score_turtle.write("Score: {}".format(score), move = False, align="center", font=FONT)
+    turtles.onclick(hand_click)
 
-
-    t.onclick(handle_click)
-    t.penup()
-    t.shape("turtle")
-    t.shapesize(2,2)
-    t.color('green')
-    t.goto(x * grid_size, y * grid_size)
-    turtle_list.append(t)
-
-x_coordinates = [-20, -10, 0, 10 ,20]
-y_coordinates = [-20, -10, 0, 10 ,20]
-
-def setup_turtles():
-    for x in x_coordinates:
-        for y in y_coordinates:
-            make_turtle(x, y)
-
-def hide_turtles():
-    for t in turtle_list: #kaplumbağaları gizle
-        t.hideturtle()
-
-def show_turtles_randomly():
+def move_turtle_randomly():
     if not game_over:
-        hide_turtles()#öncekini gizle
-        random.choice(turtle_list).showturtle()
-        turtle_screen.ontimer(show_turtles_randomly, 350) #saniyede bir kaplumbağanın yeri değişsin
+        turtles.hideturtle()
+
+        x = random.randint(-300, 300)
+        y = random.randint(-300, 300)
+        turtles.goto(x, y)
+        turtles.showturtle()
+        turtle_screen.ontimer(move_turtle_randomly, 500)#x milisaniyede bir hareket etsin
 
 def countdown(time):
     global game_over
@@ -82,7 +68,7 @@ def countdown(time):
     else:
         game_over = True
         countdown_turtle.clear()
-        hide_turtles()
+        turtles.hideturtle()
         countdown_turtle.write(arg="Game Over! :(", move=False, align="center", font=FONT)
 
 
@@ -91,8 +77,7 @@ def start_game_up():
     turtle.tracer(0)#takip etmeyi bırak
     setup_score_turtle()
     setup_turtles()
-    hide_turtles()
-    show_turtles_randomly()
+    move_turtle_randomly()
     countdown(25)
     turtle.tracer(1)#takip etmeyi başlat
 
